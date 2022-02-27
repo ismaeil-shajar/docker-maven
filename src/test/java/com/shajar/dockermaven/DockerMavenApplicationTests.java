0cd72279068b591fdda36a717f9ae4be548d72a1
ismaeil-shajar <msherif.ali6@gmail.com>
2022-02-27 21:51:57 +0200

    Initial commit

---
+++ src/test/java/com/shajar/dockermaven/DockerMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shajar.dockermaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
